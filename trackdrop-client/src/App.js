ea3d08f7adfd5023f8a64e25e245bedb052bf059
Dave Gerlach <dave@trackdrop.io>
2017-12-24 15:48:26 -0600

    trackdrop-client: Add simple mockup of original site
    Add a simple reactjs based version of the site with a single fixed track
    in preparation of hitting the new API.

--- trackdrop-client/src/App.js
+++ trackdrop-client/src/App.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import ReactWavesurfer from 'react-wavesurfer';
+import TrackBox from './components/trackbox.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      pos: 0
+    };
+    this.handleTogglePlay = this.handleTogglePlay.bind(this);
+    this.handlePosChange = this.handlePosChange.bind(this);
+  }
+  handleTogglePlay() {
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+  handlePosChange(e) {
+    this.setState({
+      pos: e.originalArgs[0]
+    });
+  }
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
           <Navbar navbar navbar-default navbar-fixed-top>
           <Navbar.Header className="navbar-header">
@@ -16,9 +39,11 @@ class App extends Component {
           </Navbar.Header>
         </Navbar>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <p className="App-intro">
+            <TrackBox />
+          </p>
+      </div>
       </div>
     );
   }
---
+++ trackdrop-client/src/components/trackbox.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import ReactWavesurfer from 'react-wavesurfer';
+
+class TrackBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      pos: 0
+    };
+    this.handleTogglePlay = this.handleTogglePlay.bind(this);
+    this.handlePosChange = this.handlePosChange.bind(this);
+  }
+
+  handleTogglePlay() {
+    this.setState({
+      playing: !this.state.playing
+    });
+    console.log("playing");
+  }
+
+  handlePosChange(e) {
+    this.setState({
+      pos: e.originalArgs[0]
+    });
+  }
+
+  render() {
+    return (
+        <div className="row" id="track<?php echo $sub_meta_info['track_id'] ?>">
+        <div className = "trackbox-header">
+            <div className="col-sm-5">
+                Track Name
+            </div>
+            <div className = "col-sm-4">
+                <button className="btn btn-primary btn-xs" onClick={this.handleTogglePlay}>
+                    <i className="fa fa-play"></i>
+                    Play /
+                    <i className="fa fa-pause"></i>
+                    Pause
+                </button>
+                <a className="btn btn-success btn-xs" href="calls.php?a=download&id=<?php echo $sub_meta_info['track_id'] ?>">
+                    <i className="fa fa-download"></i>
+                    Download
+                </a>
+            </div>
+            <div className="col-sm-3">
+                <div className="delete-button" id = "delete-div-<?php echo $sub_meta_info['track_id'] ?>">
+                    <button className="btn btn-xs btn-danger pull-right" onclick="deleteThis(track<?php echo $sub_meta_info['track_id'] ?>, <?php echo $sub_meta_info['track_id'] ?>)">
+                    <i className="fa fa-trash"></i>
+                    Delete
+                    </button>
+                </div>
+                <div className="delete-confirm pull-right" id = "delete-confirm-div-<?php echo $sub_meta_info['track_id'] ?>">
+                    Are you sure?
+                    <button className="btn btn-xs btn-success" onclick="confirmDelete(track<?php echo $sub_meta_info['track_id'] ?>, <?php echo $sub_meta_info['track_id'] ?>)">
+                    Yes
+                    </button>
+                    <button className="btn btn-xs btn-danger" onclick="cancelDelete(<?php echo $sub_meta_info['track_id'] ?>)">
+                    No
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className = "trackbox-player-wrap">
+            <div className="col-sm-12">
+                <div className="trackbox-player">
+                    <ReactWavesurfer className=""
+                    audioFile={'http://trackdrop.io/stream/b3a5b221ad611bb73a0cd9fafea96e43f27cab280100744a907fd4c40e91c404.mp3'}
+                    pos={this.state.pos}
+                    onPosChange={this.handlePosChange}
+                    playing={this.state.playing}
+                    options={{
+                        waveColor: 'violet',
+                        progressColor: 'purple',
+                        barWidth: 1,
+                        hideScrollbar: true,
+                        preload: "metadata",
+                        backend: 'MediaElement',
+                        mediaType:'audio'
+                    }}
+                    />
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TrackBox;
