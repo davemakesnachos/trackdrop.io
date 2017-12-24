35c1aa3019742d43b2449d4edb86ae65b034643e
Dave Gerlach <dave@trackdrop.io>
2017-12-23 23:38:00 -0600

    trackdrop-client: App: Add basic header and scss
    Add the old trackdrop header to the main App.

--- trackdrop-client/src/App.js
+++ trackdrop-client/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Navbar navbar navbar-default navbar-fixed-top>
+          <Navbar.Header className="navbar-header">
+            <Navbar.Brand>
+              <a class="navbar-brand" href="/">track<span class = "navbar-brand-accent">drop</span>.io</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
