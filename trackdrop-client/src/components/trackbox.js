d39b194bb9e7acb784a9ab24fc26fa528b7ba4bc
Dave Gerlach <dave@trackdrop.io>
2017-12-28 15:01:30 -0600

    trackdrop-client: trackbox: Remove legacy bootstrap code
    We aren't using bootstrap anymore so drop the legacy code.

--- trackdrop-client/src/components/trackbox.js
+++ trackdrop-client/src/components/trackbox.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Panel, Row, Col } from 'react-bootstrap';
 import {
     Card,
     Divider,
@@ -47,64 +46,6 @@ class TrackBox extends Component {
     })
   }
   render() {
-    let hideDelete = () => this.setState({ deleteShow: false });
-
-    const tmp = (
-        <div className="row" id="track<?php echo $sub_meta_info['track_id'] ?>">
-        <Panel>
-        <div className = "trackbox-player-wrap">
-            <div className="col-sm-12">
-                <div className="trackbox-player">
-                    <ReactWavesurfer className=""
-                    audioFile={this.props.track.streamUrl}
-                    pos={this.state.pos}
-                    onPosChange={this.handlePosChange}
-                    playing={this.state.playing}
-                    options={{
-                        waveColor: 'violet',
-                        progressColor: 'purple',
-                        barWidth: 3,
-                        hideScrollbar: true,
-                        preload: "metadata",
-                        backend: 'MediaElement',
-                        mediaType:'audio'
-                    }}
-                    />
-                </div>
-            </div>
-        </div>
-        </Panel>
-      </div>
-    )
-
-    const panelHeaderACtual = (
-        <div className = "row trackbox-header">
-            <div className="col-sm-5">
-                {this.props.track.name}
-            </div>
-            <div className = "col-sm-4">
-                <button className="btn btn-primary btn-xs" onClick={this.handleTogglePlay}>
-                    <i className="fa fa-play"></i>
-                    Play /
-                    <i className="fa fa-pause"></i>
-                    Pause
-                </button>
-                <a className="btn btn-success btn-xs" href={this.props.track.downloadUrl}>
-                    <i className="fa fa-download"></i>
-                    Download
-                </a>
-            </div>
-            <div className="col-sm-3">
-                <div className="delete-button" id = "delete-div-<?php echo $sub_meta_info['track_id'] ?>">
-                    <button className="btn btn-xs btn-danger pull-right" onClick={() => this.setState({ deleteShow: true })}>
-                    <i className="fa fa-trash"></i>
-                    Delete
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-
     return (
         <Card fluid>
         <Card.Content>
@@ -144,7 +85,7 @@ class TrackBox extends Component {
             </Grid.Column>
             </Grid.Row>
             </Grid>
-            <DeleteButton show={this.state.deleteShow} track={this.props.track} onHide={hideDelete} confirmdelete={this.confirmDelete}/>
+            <DeleteButton show={this.state.deleteShow} track={this.props.track} confirmdelete={this.confirmDelete}/>
             <Button color='green' size="mini" href={this.props.track.downloadUrl} floated='right'>Download</Button>
         </Card.Content>
       </Card>
