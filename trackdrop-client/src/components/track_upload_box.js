13fbf5f2b602725f5c3c5d8330b43f83c3641d2f
Dave Gerlach <dave@trackdrop.io>
2017-12-31 12:11:16 -0600

    trackbox-client: trackUploadBox: Move to using the new upload API endpoint
    Let's now use the new API endpoint to upload our track and define a
    success handler to be able to catch the track when it uploads
    successfully.

--- trackdrop-client/src/components/track_upload_box.js
+++ trackdrop-client/src/components/track_upload_box.js
@@ -4,16 +4,28 @@ import DropzoneComponent from 'react-dropzone-component';
 var componentConfig = {
     iconFiletypes: ['.mp3'],
     showFiletypeIcon: true,
-    postUrl: '/upload.php'
+    postUrl: '/api/v1/track/upload'
 };
 
 var djsConfig = { autoProcessQueue: true }
 
 export class TrackUploadBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.eventHandlers = {
+            success: this.success,
+        }
+    }
+
+    success(file, response) {
+    }
+
     render() {
         return (<div>
                 <DropzoneComponent config={componentConfig}
-                            djsConfig={djsConfig} />
+                            djsConfig={djsConfig}
+                            eventHandlers={this.eventHandlers} />
             </div>);
     }
 }
