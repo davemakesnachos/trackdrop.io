7c7552d3f176fd0842b311649d69e022aec1f8b1
Dave Gerlach <dave@trackdrop.io>
2019-03-30 13:01:43 -0500

    model: user: Add better error catching and exception handling
    Actually process errors and catch PDOExceptions so that we can return
    meaningful data to the caller.

--- app/model/user.model.php
+++ app/model/user.model.php
@@ -15,6 +15,33 @@ class UserModel extends Model
 
     public function create($user_data)
     {
+        if ($user_data['email'] == "") {
+            $error['status'] = 'fail';
+            $error["error"] = "The 'email' field must be provided.";
+            $error["field"] = "email";
+        }
+
+        if ($user_data['password'] == "") {
+            $error['status'] = 'fail';
+            $error["error"] = "The 'password' field must be provided.";
+            $error["field"] = "password";
+        }
+
+        if ($user_data['name'] == "") {
+            $error['status'] = 'fail';
+            $error["error"] = "The 'name' field must be provided.";
+            $error["field"] = "name";
+        }
+
+        if ($user_data['code'] == "") {
+            $error['status'] = 'fail';
+            $error["error"] = "The 'code' field must be provided.";
+            $error["field"] = "code";
+        }
+
+        if (isset($error))
+            return $error;
+
         if (isset($user_data['password']))
             $user_data['password_hash'] = password_hash($user_data['password'], PASSWORD_DEFAULT);
         else
@@ -25,7 +52,34 @@ class UserModel extends Model
                                                              'password_hash',
                                                              'level',
                                                              'status',));
-        return $this->insert($user_data_cleaned);
+
+        try {
+            $insert_id = $this->insert($user_data_cleaned);
+        } catch (\PDOException $e) {
+            $error['status'] = 'fail';
+            switch($e->getCode()) {
+                case "23000":
+                    $str = $e->getMessage();
+                    if (strpos($str, 'email') !== FALSE) {
+                        $error["error"] = "This email is already in use.";
+                        $error["field"] = "email";
+                    } else if (strpos($str, 'name') !== FALSE) {
+                        $error["error"] = "This name is already in use.";
+                        $error["field"] = "name";
+                    } else {
+                        $error["error"] = "Unknown Error.";
+                    }
+                    break;
+                default:
+                    $error["error"] = "Unknown Error.";
+                    break;
+            }
+
+            return $error;
+        }
+        $result['user_id'] = $insert_id;
+        $result['status'] = 'OK';
+        return $result;
     }
 
     public function authenticate($password)
