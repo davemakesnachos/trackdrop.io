12b0405d4cf1ca2dc111c7884d23cfd53dbf7f7c
Dave Gerlach <dave@trackdrop.io>
2019-04-07 17:44:39 -0500

    controller: track: Populate user data for trackList during API call
    Provide user data with the track data when fetching all tracks from the
    server.

--- app/controller/api/v1/track.controller.php
+++ app/controller/api/v1/track.controller.php
@@ -5,6 +5,7 @@ namespace App\Controller\Api\v1;
 use Core\Controller;
 use App\Model\TrackModel;
 use App\Model\TrackWaveDataModel;
+use App\Model\UserModel;
 
 class TrackController extends Controller
 {
@@ -29,6 +30,13 @@ class TrackController extends Controller
                     $t->wave_data = json_decode($d->data);
                 $t->streamUrl = SITE_URL . "/stream/" . $t->hash . ".mp3";
                 $t->downloadUrl = SITE_URL . "/track/download/" . $t->id;
+
+                /* Populate user data */
+
+                $u = UserModel::find($t->user_id);
+                if (isset($u))
+                    $t->user = $u->name;
+
                 $track_list[] = $t;
             }
         }
