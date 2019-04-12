310ded11527cf6864320cad7a590452412b33f47
Dave Gerlach <dave@trackdrop.io>
2019-04-11 19:34:25 -0500

    trackdrop-client: Convert login and register components to hooks
    Hooks are here and they are cool, so convert the login and register
    components to use them instead. State management can easily be
    constrained to local scope, so let's do it this way for now. Still will
    need redux probably but that's ok!

--- trackdrop-client/src/components/login.js
+++ trackdrop-client/src/components/login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
     Button,
     Grid,
@@ -6,23 +6,63 @@ import {
     Segment,
     Form,
 } from 'semantic-ui-react'
+import { userService } from '../lib/user.js'
 
-export class Login extends Component {
-    render() {
-        return (
-            <div className="login-form">
-                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-                    <Grid.Column style={{ maxWidth:'450px'}} >
-                        <Form size = "large">
-                            <Segment stacked>
-                                <Form.Input fluid icon="user" iconPosition="left" placeholder="Email Address" />
-                                <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" type='password' />
-                                <Button size="large">Login</Button>
-                            </Segment>
-                        </Form>
-                    </Grid.Column>
-               </Grid>
-            </div>
-        )
+export function Login(props) {
+
+    const initialState = {
+        username: "",
+        password: "",
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const [state, setState] = useState(initialState)
+
+    const doLogin = () => {
+        setIsLoading(true);
+        setError("");
+        userService.login(state)
+            .then(function (response) {
+                setTimeout(() => {
+                    props.history.push('/tracks');
+                }, 1000)
+
+            })
+            .catch(function (error) {
+                setIsLoading(false);
+                console.log(error.response)
+                setError(error.response.data.msg);
+            });
+
     }
-  }
+
+    const handleInputChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div className="login-form">
+            <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth:'450px'}} >
+                    <Form size = "large">
+                        <Segment stacked>
+                            <Form.Input fluid icon="user" iconPosition="left" placeholder="Email or Profile Name" name="username" value={state.username} onChange={handleInputChange} />
+                            <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" type='password' name="password" value={state.password} onChange={handleInputChange} />
+                            { (error !== "")
+                              ? (<div className="ui negative message">
+                                    <div className="header">Please fix the following</div>
+                                    <p>{error}</p>
+                                </div>)
+                              : "" }
+                            { isLoading
+                              ? <Button size="large" loading>Login</Button>
+                              : <Button size="large" onClick={ doLogin }>Login</Button> }
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+            </Grid>
+        </div>
+    )
+}
--- trackdrop-client/src/components/register.js
+++ trackdrop-client/src/components/register.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
     Button,
     Grid,
@@ -6,25 +6,67 @@ import {
     Segment,
     Form,
 } from 'semantic-ui-react'
+import { userService } from '../lib/user.js'
 
-export class Register extends Component {
-    render() {
-        return (
-            <div className="register-form">
-                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-                    <Grid.Column style={{ maxWidth:'450px'}} >
-                        <Form size = "large">
-                            <Segment stacked>
-                                <Form.Input fluid icon="mail" iconPosition="left" placeholder="Email Address" />
-                                <Form.Input fluid icon="user" iconPosition="left" placeholder="Name" />
-                                <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" type='password' />
-                                <Form.Input fluid icon="lock" iconPosition="left" placeholder="Access Code" />
-                                <Button size="large">Register</Button>
-                            </Segment>
-                        </Form>
-                    </Grid.Column>
-               </Grid>
-            </div>
-        )
+export function Register(props) {
+
+    const initialState = {
+        email: "",
+        name: "",
+        password: "",
+        code: "",
+    };
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [registrationError, setRegistrationError] = useState("");
+
+    const [state, setState] = useState(initialState)
+
+    const doRegister = () => {
+        setIsLoading(true);
+        setRegistrationError("");
+        userService.register(state)
+            .then(function (response) {
+                setTimeout(() => {
+                    props.history.push('/tracks');
+                }, 1000)
+
+            })
+            .catch(function (error) {
+                setIsLoading(false);
+                console.log(error.response)
+                setRegistrationError(error.response.data.msg);
+            });
+
     }
-  }
+
+    const handleInputChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <div className="register-form">
+            <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth:'450px'}} >
+                    <Form size = "large">
+                        <Segment stacked>
+                            <Form.Input fluid icon="mail" iconPosition="left" placeholder="Email Address" name="email" value={ state.email } onChange = { handleInputChange } />
+                            <Form.Input fluid icon="user" iconPosition="left" placeholder="Name" name="name" value={ state.name } onChange={ handleInputChange } />
+                            <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" name="password" type='password' value={ state.password } onChange={ handleInputChange } />
+                            <Form.Input fluid icon="lock" iconPosition="left" placeholder="Access Code" name="code" value={ state.code } onChange={ handleInputChange } />
+                            { (registrationError !== "")
+                              ? (<div className="ui negative message">
+                                    <div className="header">Please fix the following</div>
+                                    <p>{registrationError}</p>
+                                </div>)
+                              : "" }
+                            { isLoading
+                              ? <Button size="large" loading>Register</Button>
+                              : <Button size="large" onClick={ doRegister }>Register</Button> }
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+            </Grid>
+        </div>
+    )
+}
