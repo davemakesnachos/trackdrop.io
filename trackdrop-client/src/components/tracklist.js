0ead73ec1687bc7ebe9f47361ab483cc0b66b488
Dave Gerlach <dave@trackdrop.io>
2019-04-21 21:48:40 -0500

    trackdrop-client: tracklist: Handle empty initial tracklist
    Sometimes the tracklist will be empty, and even more commonly now
    because users can create accounts and may not have any tracks available
    immediately, so we must handle the case where none are available by
    checking the number of tracks we've pulled and displaying a message if
    nothing is there.
    
    NOTE: Message must still be styled.

--- trackdrop-client/src/components/tracklist.js
+++ trackdrop-client/src/components/tracklist.js
@@ -5,11 +5,14 @@ export class TrackList extends Component {
     render() {
       let trackList = this.props.tracks;
       let removetrack = this.props.removetrack;
+      if (trackList.length > 0) {
+        const trackListRendered = Object.keys(trackList).map(function(key) {
+          return <TrackBox key={trackList[key].id} track={trackList[key]} removetrack={removetrack} />;
+        });
 
-      const trackListRendered = Object.keys(trackList).map(function(key) {
-        return <TrackBox key={trackList[key].id} track={trackList[key]} removetrack={removetrack} />;
-      });
-
-      return (<span> {trackListRendered} </span>);
+        return (<span> {trackListRendered} </span>);
+      } else {
+        return (<span> Upload some tracks! </span>);
+      }
     }
   }
