4b4b2530a7738aba2fc5109dc2de6c949166e7ed
Dave Gerlach <dave@trackdrop.io>
2020-03-16 17:23:26 -0500

    controller: user: Change code checking for invites
    Invite code checking does not make sense in the User model and has now
    been removed. Instead enforce the requirement in the Controller since we
    are using another model to manage that. We must ensure invite code is
    provided for user registration so accounts cannot be created without it.

--- app/controller/api/v1/user.controller.php
+++ app/controller/api/v1/user.controller.php
@@ -8,6 +8,7 @@ namespace App\Controller\Api\v1;
 
 use Core\Controller;
 use App\Model\UserModel;
+use App\Model\InviteCodeModel;
 
 class UserController extends Controller
 {
@@ -15,17 +16,39 @@ class UserController extends Controller
     public function register()
     {
         $user = new UserModel();
+        $inviteCode = new InviteCodeModel();
 
         $json_data_input = $this->params['json'];
         $user_data = array("email" => $json_data_input['email'],
                            "password" => $json_data_input['password'],
-                           "name" => $json_data_input['name'],
-                           "code" => $json_data_input['code']);
+                           "name" => $json_data_input['name']);
+
+
+        if (!array_key_exists('code', $json_data_input) || $json_data_input['code'] == "") {
+            $result['status'] = '400';
+            $result["error"] = "The 'code' field must be provided.";
+            $result["field"] = "code";
+            $response = json_response_fail(400, [], $result['error']);
+            $this->data('response', $response);
+            return;
+        } else {
+            $code = $json_data_input['code'];
+        }
+
+        $result = $inviteCode->check($code);
+        if ($result == NULL) {
+            $response = json_response_fail(400, [], 'Invalid invite code provided!');
+            $result["field"] = "code-validation";
+            $this->data('response', $response);
+            return;
+        }
 
         $result = $user->create($user_data);
         if ($result['status'] == "fail") {
             $response = json_response_fail(400, [], $result['error']);
         } else {
+            // XXX: Must check error
+            $inviteCode->markUsed($code);
             $response = json_response_success(["id" => $result['user_id'] ]);
         }
         $this->data('response', $response);
