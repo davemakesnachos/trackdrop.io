f28c2b26f70e0691f7a2ffb74c3a49cba5ea3d10
Dave Gerlach <dave@trackdrop.io>
2020-06-07 13:04:06 -0500

    config: routes: Add APIDoc style comments
    The Web API is becoming somewhat complex, so add APIDoc style comments
    so the endpoints can be documented. Also, add a Makefile under the docs/
    path so that documentation can be built by do:
    
      $ cd docs
      $ make
    
    Which will provide API docs under docs/apidoc.

--- config/routes/api/v1.php
+++ config/routes/api/v1.php
@@ -2,11 +2,27 @@
 
 use Core\Router;
 
+/**
+ * @api {get} api/v1/tracks Get all tracks in stream
+ * @apiName TracksAll
+ * @apiGroup Tracks
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ */
+
 Router::addApi([
     'url' => '/api/v1/tracks',
     'controller' => 'TrackController',
     'method' => 'allTracks',
 ]);
+
+/**
+ * @api {get} api/v1/tracks/{profile} Get all tracks for a profile
+ * @apiName TracksProfile
+ * @apiGroup Tracks
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ */
 
 Router::addApi([
     'url' => '/api/v1/tracks/{profile}',
@@ -14,11 +30,27 @@ Router::addApi([
     'method' => 'allTracksForProfile',
 ]);
 
+/**
+ * @api {post} api/v1/track/upload Upload a track
+ * @apiName TracksUpload
+ * @apiGroup Tracks
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ */
+
 Router::addApi([
     'url' => '/api/v1/track/upload',
     'controller' => 'TrackController',
     'method' => 'upload',
 ]);
+
+/**
+ * @api {post} api/v1/track/delete Delete a track
+ * @apiName TracksDelete
+ * @apiGroup Tracks
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ */
 
 Router::addApi([
     'url' => '/api/v1/track/delete',
@@ -26,18 +58,53 @@ Router::addApi([
     'method' => 'delete',
 ]);
 
+/**
+ * @api {get} api/v1/track/{profile}/{track} Get a single track associated with profile
+ * @apiName TracksProfileTrack
+ * @apiGroup Tracks
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ */
+
 Router::addApi([
     'url' => '/api/v1/track/{profile}/{track}',
     'controller' => 'TrackController',
     'method' => 'trackFromProfileAndName',
 ]);
 
-# User Routes
+/**
+ * @api {get} api/v1/user/register Register a new user
+ * @apiName UserRegister
+ * @apiGroup User
+ *
+ * @apiParam {String} email User's email address they will use to login.
+ * @apiParam {String} name User's name for their profile.
+ * @apiParam {String} password User's new password they will use to login.
+ * @apiParam {String} code Invite code that is currently required to get an account.
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ * @apiError (400) {String} result 'Invalid invite code provided!'
+ */
+
 Router::addApi([
     'url' => '/api/v1/user/register',
     'controller' => 'UserController',
     'method' => 'register',
 ]);
+
+/**
+ * @api {post} api/v1/user/reset_password Do a password reset
+ * @apiName UserResetPassword
+ * @apiGroup User
+ *
+ * @apiParam {String} token ID created by forgot password request that was received in email.
+ * @apiParam {String} password User's new password they will use to login.
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ * @apiError (404) {String} result 'Invalid token.'.
+ * @apiError (400) {String} result 'Invalid.'.
+ * @apiError (400) {String} result 'Operation Failed.'.
+ */
 
 Router::addApi([
     'url' => '/api/v1/user/reset_password',
@@ -45,11 +112,33 @@ Router::addApi([
     'method' => 'reset_password',
 ]);
 
+/**
+ * @api {post} api/v1/login Login a user
+ * @apiName UserLogin
+ * @apiGroup User
+ *
+ * @apiParam {String} email User's email address they will use to login.
+ * @apiParam {String} password User's password they will use to login.
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ * @apiError (400) {String} result 'User and password combination not found in database.'
+ * @apiError (400) {String} result 'Not Logged In.'
+ */
+
 Router::addApi([
     'url' => '/api/v1/login',
     'controller' => 'SessionController',
     'method' => 'login',
 ]);
+
+/**
+ * @api {get} api/v1/logout Logout a user
+ * @apiName UserLogout
+ * @apiGroup User
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ * @apiError (404) {String} result 'Invalid session token.'.
+ */
 
 Router::addApi([
     'url' => '/api/v1/logout',
@@ -57,12 +146,32 @@ Router::addApi([
     'method' => 'logout',
 ]);
 
-# Invite Routes
+/**
+ * @api {post} api/v1/invite/create Create an invite
+ * @apiName InviteCreate
+ * @apiGroup Invite
+ *
+ * @apiParam {String} email Email address to send invite to.
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ * @apiError (401) {String} result 'Password reset request failed. Please try again later.'
+ */
+
 Router::addApi([
     'url' => '/api/v1/invite/create',
     'controller' => 'InviteController',
     'method' => 'create',
 ]);
+
+/**
+ * @api {post} api/v1/invite/validate Validate an invite
+ * @apiName InviteValidate
+ * @apiGroup Invite
+ *
+ * @apiParam {String} code Token generated with a call to InviteCreate that will allow access to register.
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ */
 
 Router::addApi([
     'url' => '/api/v1/invite/validate',
@@ -70,12 +179,33 @@ Router::addApi([
     'method' => 'validate',
 ]);
 
-# Password reset routes
+/**
+ * @api {post} api/v1/forgot_password Create a forgot password request
+ * @apiName PasswordResetRequestCreate
+ * @apiGroup PasswordResetRequest
+ *
+ * @apiParam {String} email Email address associated with an account that will create a password request
+ *
+ * @apiSuccess (200) {String} result 'OK'.
+ * @apiError (400) {String} result 'Password reset request failed. Please try again later.'
+ */
+
 Router::addApi([
     'url' => '/api/v1/forgot_password',
     'controller' => 'PasswordResetRequestController',
     'method' => 'create',
 ]);
+
+/**
+ * @api {post} api/v1/reset_password Validate a check password request token
+ * @apiName PasswordResetRequestValidate
+ * @apiGroup PasswordResetRequest
+ *
+ * @apiParam {String} token Token generated by a call to PasswordResetRequestCreate.
+ *
+ * @apiSuccess (200) {String} result 'OK'
+ * @apiError (404) {String} result 'Invalid token.'
+ */
 
 Router::addApi([
     'url' => '/api/v1/reset_password',
